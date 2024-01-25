77a8a6429c9301757bd5cbb0f79c7cadcd92c1cf
AceMortam <portag2b@gmail.com>
2024-01-25 11:03:33 +0100

    Changed the HTML for the site.

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 25 10:21:55 2024
+
+@author: guill
+"""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
